1346440c28b18638373a2cb53cfe6ad803ede4a8
Dominique <dc@webformation.fr>
2020-03-05 19:16:07 +0100

    ajout getValeur et estValide

--- capteurTemperature.h
+++ capteurTemperature.h
@@ -35,6 +35,21 @@ public:
         }
         thermometre(num,1);
     }
+    float getValeur() {
+        int val;
+        if ((val=thermometre(num,2)) < 0)
+        {
+            throw runtime_error("echec lecture du thermometre");
+        }
+        else {
+            return static_cast<float>(val);
+        }
+    }
+    bool estValide() {
+        if (!controle) return true;
+        float v = getValeur();
+        return (v>= min && v <= max);
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -4,19 +4,10 @@ using namespace std;
 #include "capteurTemperature.h"
 
 
-int main() {
-    try {
-        capteurTemperature c1("es",2);
-        cout << *(c1.nom) << endl;
-    } catch( invalid_argument e) {
-        cout << e.what() << endl;
-    }
-    catch( invalid_argument * pe) {
-        cout << pe->what() << endl;
-        delete pe;
-    }
-    catch (int i) {
-        cout << i << endl;
-    }
+int main()
+{
+    capteurTemperature c1("essai",2);
+    cout << *(c1.nom) << endl;
+    cout << boolalpha << c1.estValide() << endl;
     return 0;
 }
