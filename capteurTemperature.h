607573683d0820f9a8f349972cf36f8faa3c3d70
Dominique <dc@webformation.fr>
2020-03-06 09:09:17 +0100

    ajout ctor copy et assign copy

--- capteurTemperature.h
+++ capteurTemperature.h
@@ -27,6 +27,24 @@ public:
             throw runtime_error("echec initialisation du thermometre");
         }
     }
+    capteurTemperature(const capteurTemperature & ct): min(ct.min),  max(ct.max)
+    {
+        nom = new string(*(ct.nom));
+        num = ct.num;
+        controle = ct.controle;
+    }
+    capteurTemperature & operator=(const capteurTemperature & ct)
+    {
+        if (this == &ct) return *this;
+        if (nom!=nullptr)
+            delete nom;
+        nom = new string(*(ct.nom));
+        num = ct.num;
+        min = ct.min;
+        max = ct.max;
+        controle = ct.controle;
+        return *this;
+    }
     ~capteurTemperature()
     {
         if (nom != nullptr)
@@ -35,26 +53,35 @@ public:
         }
         thermometre(num,1);
     }
-    float getValeur() {
+    float getValeur()
+    {
         int val;
         if ((val=thermometre(num,2)) < 0)
         {
             throw runtime_error("echec lecture du thermometre");
         }
-        else {
+        else
+        {
             return static_cast<float>(val);
         }
     }
-    bool estValide() {
-        if (!controle) return true;
+    bool estValide()
+    {
+        if (!controle)
+            return true;
         float v = getValeur();
         return (v>= min && v <= max);
     }
-    bool operator==(const capteurTemperature & ct) {
-        if (this == &ct) return true;
+    bool operator==(const capteurTemperature & ct)
+    {
+        if (this == &ct)
+            return true;
         return (*nom == *(ct.nom) && num==ct.num);
     }
-    string getNom() const { return *nom;}
+    string getNom() const
+    {
+        return *nom;
+    }
     friend ostream & operator<<(ostream & out, const capteurTemperature& c);
 };
 
