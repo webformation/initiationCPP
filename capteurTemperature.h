91bf9eda1a8141c63cbe12f2cbd50ef6ff1189db
Dominique <dc@webformation.fr>
2020-03-10 10:55:12 +0100

    capture des exceptions

--- capteurTemperature.h
+++ capteurTemperature.h
@@ -14,13 +14,17 @@ public:
     {
         controle = false;
     }
-    capteurTemperature(string nom, int num, float min, float max) : num(num), min(min), max(max),controle(true)
+    capteurTemperature(string nom, int num, float min, float max)  : num(num), min(min), max(max),controle(true)
     {
         if (nom.size() < 3)
         {
+            // éviter de créer une exception avec new, car cela oblige à penser au delete dans le catch
+            //throw new invalid_argument("le nom doit contenir au moins 3 caracteres");
             throw invalid_argument("le nom doit contenir au moins 3 caracteres");
+
         }
         this->nom = new string(nom);
+        //this->nom = &nom; // mauvaise idée car on ne connait pas la durée de vie de nom
 
     }
     ~capteurTemperature()
--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@ using namespace std;
 #include "capteurTemperature.h"
 
 
-int main()
-{
-    capteurTemperature c1("essai",2);
-    cout << *(c1.nom) << endl;
+int main() {
+    try {
+        capteurTemperature c1("es",2);
+        cout << *(c1.nom) << endl;
+    } catch( invalid_argument e) {
+        cout << e.what() << endl;
+    }
+    catch( invalid_argument * pe) {
+        cout << pe->what() << endl;
+        delete pe;
+    }
+    catch (int i) {
+        cout << i << endl;
+    }
     return 0;
 }
